37a6ec5e7d636b821bd6cdb3cccf30e10d1bbcf1
janjoris <jan.de.dobbeleer@gmail.com>
2015-07-27 17:52:25 +0200

    - Added Mock ability

---
+++ Monizze.Api/Client/MockClient.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Monizze.Api.Model;
+
+namespace Monizze.Api.Client
+{
+    public class MockClient : IMonizzeClient
+    {
+        public Task<bool> Login(string email, string password)
+        {
+            return new Task<bool>(() => true);
+        }
+
+        public Task<Account> GetAccount()
+        {
+           return new TaskFactory().StartNew(() => new Account { Balance = "30.39", CardNumbers = new List<string> { "32321313131", "3323424242" }, FirstName = "Test", LastName = "User" });
+            //return new Task<Account>(() => new Account { Balance = "30.39", CardNumbers = new List<string> { "32321313131", "3323424242" }, FirstName = "Test", LastName = "User" });
+        }
+
+        public Task<List<Transaction>> GetTransactions(string transactionId = "0")
+        {
+            var list = new List<Transaction>
+            {
+                new Transaction
+                {
+                    Amount = "13.90",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.91",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.92",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.93",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.94",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.95",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.96",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.97",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.98",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "13.99",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.00",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.10",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.20",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.30",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.40",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                },
+                new Transaction
+                {
+                    Amount = "14.50",
+                    MerchantName = "This is a merchant",
+                    TimeStamp = "12-09-2014",
+                    Transactionid = "1"
+                }
+            };
+            return new TaskFactory().StartNew(() => list);
+            //return new Task<List<Transaction>>(() => list);
+        }
+    }
+}
--- Monizze/ViewModel/ViewModelLocator.cs
+++ Monizze/ViewModel/ViewModelLocator.cs
@@ -50,38 +50,24 @@ namespace Monizze.ViewModel
             SimpleIoc.Default.Register<ICredentialManager, CredentialManager>();
             SimpleIoc.Default.Register<ILogger, Logger>();
             SimpleIoc.Default.Register<IDeviceInfo, DeviceInfo>();
+#if MOCK
+            SimpleIoc.Default.Register<IMonizzeClient, MockClient>();
+#else
             SimpleIoc.Default.Register<IMonizzeClient, MonizzeClient>();
+#endif
             SimpleIoc.Default.Register<MainViewModel>();
             SimpleIoc.Default.Register<LoginViewModel>();
             SimpleIoc.Default.Register<AccountViewModel>();
             
         }
 
-        public MainViewModel MainViewModel
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
-        
-        public LoginViewModel LoginViewModel
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<LoginViewModel>();
-            }
-        }
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
 
-        
-        public AccountViewModel AccountViewModel
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<AccountViewModel>();
-            }
-        }
-        
+        public LoginViewModel LoginViewModel => ServiceLocator.Current.GetInstance<LoginViewModel>();
+
+
+        public AccountViewModel AccountViewModel => ServiceLocator.Current.GetInstance<AccountViewModel>();
+
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
